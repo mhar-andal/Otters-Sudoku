8f8ae16bf1b9152e31d49c61533a07fed05e9e34
Mhar Andal <mharandal96@gmail.com>
2016-12-09 14:21:45 -0600

    Write pseudocode

---
+++ sudoku.rb
@@ -0,0 +1,53 @@
+# rreturns each row
+# array.scan(/.{9}/).flatten
+
+#
+
+# 5 3 0 0 7 0 0 0 0
+# 6 0 0 1 9 5 0 0 0
+# 0 9 8 0 0 0 0 6 0
+# 8 0 0 0 6 0 0 0 3
+# 4 0 0 8 0 3 0 0 1
+# 7 0 0 0 2 0 0 0 6
+# 0 6 0 0 0 0 2 8 0
+# 0 0 0 4 1 9 0 0 5
+# 0 0 0 0 8 0 0 7 9
+
+# 5 3 0 0 7 0 0 0 0
+# 6 0 0 1 9 5 0 0 0
+# 0 9 8 0 0 0 0 6 0
+# 8 0 0 0 6 0 0 0 3
+# 4 0 0 8 0 3 0 0 1
+# 7 0 0 0 2 0 0 0 6
+# 0 6 0 0 0 0 2 8 0
+# 0 0 0 4 1 9 0 0 5
+# 0 0 0 0 8 0 0 7 9
+
+def solve(board)
+  max_value = 9
+  grid_size = 9
+
+
+
+
+  while !solved?
+
+
+  end
+end
+
+def solved?
+
+end
+
+def check_row?
+
+end
+
+def check_col?
+
+end
+
+def check_grid?
+
+end
