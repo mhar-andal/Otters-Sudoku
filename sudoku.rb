245c2d4c00553f0b48a6e9ed03ccef9c70b9c590
Mhar Andal <email": null>
2016-12-09 14:46:59 -0600

    Psuedocode

--- sudoku.rb
+++ sudoku.rb
@@ -1,7 +1,10 @@
 # rreturns each row
 # array.scan(/.{9}/).flatten
 
-#
+# puzzle[0..2].each{|row| row.slice(0..2)} gives grid 1
+# puzzle[3..5].each{|row| row.slice(0..2)} gives grid 4
+# puzzle[6..8].each{|row| row.slice(0..2)} gives grid 7
+
 
 # 5 3 0 0 7 0 0 0 0
 # 6 0 0 1 9 5 0 0 0
@@ -23,7 +26,12 @@
 # 0 0 0 4 1 9 0 0 5
 # 0 0 0 0 8 0 0 7 9
 
-def solve(board)
+# Takes a board as a string in the format
+# you see in the puzzle file. Returns
+# something representing a board after
+# your solver has tried to solve it.
+# How you represent your board is up to you!
+def solve(board_string)
   max_value = 9
   grid_size = 9
 
@@ -36,8 +44,19 @@ def solve(board)
   end
 end
 
-def solved?
+# Returns a boolean indicating whether
+# or not the provided board is solved.
+# The input board will be in whatever
+# form `solve` returns.
+def solved?(board)
+end
 
+# Takes in a board in some form and
+# returns a _String_ that's well formatted
+# for output to the screen. No `puts` here!
+# The input board will be in whatever
+# form `solve` returns.
+def pretty_board(board)
 end
 
 def check_row?
